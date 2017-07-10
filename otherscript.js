9f5e01279517b6c48fcd22b3de1c1a28968f3069
Hannah-K-Hunter <19hunterh22@sanmateohigh.org>
2017-07-10 13:55:14 -0700

    Update otherscript.js

--- otherscript.js
+++ otherscript.js
@@ -1,13 +1,7 @@
-$(document).ready(function(){
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-    var target = this.hash;
-	   $target = $(target);
-    $('html, body').stop().animate({
-	  'scrollTop': $target.offset().top
-	   }, 
-    900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	})
+var $root = $('html, body');
+$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
 });
