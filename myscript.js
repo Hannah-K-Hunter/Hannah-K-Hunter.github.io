e9567c3b7c04c9ca361bf59c568a9c5aa50b866f
Hannah-K-Hunter <19hunterh22@sanmateohigh.org>
2017-07-13 09:09:22 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,7 @@
-
+var canvas= document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle="Red";
+ctx.fill();
+ctx.closePath();
